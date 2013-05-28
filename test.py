ba4d5b9b1c3a12af016d51695eb5dbb49c977bbe
mcropper <matthewcropper1@Matthew-Croppers-MacBook-Pro.local>
2013-05-28 21:10:11 +0100

    added new test python file

---
+++ test.py
@@ -0,0 +1,17 @@
+for random import ranint
+board = []
+
+for i in range(0,5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board
+            print " ".join(row)
+
+def random_row():
+    return randint(0,len(board) -1)
+
+def rand_col():
+    return randint(0,len(board[0]) -1)
+
+
